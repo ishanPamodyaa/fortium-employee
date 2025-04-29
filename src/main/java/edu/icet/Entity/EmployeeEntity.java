eccef47c9d20e67605bfcc5e079178a10040e228
ishanPamodyaa <inishan9999@gmail.com>
2025-04-29 10:27:38 +0530

    update employee Entity class

--- src/main/java/edu/icet/Entity/EmployeeEntity.java
+++ src/main/java/edu/icet/Entity/EmployeeEntity.java
@@ -1,4 +1,34 @@
 package edu.icet.Entity;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.modelmapper.internal.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="employees")
+@Data
 public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false,length = 100)
+    private String name;
+    @Column(nullable = false,unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String department;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+
 }
